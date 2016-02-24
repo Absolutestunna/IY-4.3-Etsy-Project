0b94e5b9889598e469ccb602e0be5de5083319fb
Joshua Abu <joshujah007@gmail.com>
2016-02-24 16:07:54 -0500

    API pulls

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,12 +1,37 @@
 var $ = require("jquery");
 var handlebars = require('handlebars');
 
-var source   = $("#image-template").html();
+
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=soccer&includes=Images,Shop";
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
+var source   = $("#category-template").html();
 var template = handlebars.compile(source);
 
-var context = {
-  img:"https://unsplash.it/170/135"
-};
-var html = template(context);
-$(".img1").html(html);
-$(".img2").html(html);
+function logData(data){
+    var arrayData = data.results;
+    console.log(arrayData)
+    arrayData.forEach(function(value, index, thisArray){
+      var context = {
+        image: thisArray[index].Images[0].url_fullxfull,
+        price: thisArray[index].price,
+        title: thisArray[index].title,
+        currency_code: thisArray[index].currency_code,
+        manufacturer: thisArray[index].Shop.shop_name,
+      }
+      $(".main-body").append(template(context));
+    });
+}
+fetchJSONP(url, logData);
