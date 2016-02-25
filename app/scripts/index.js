d940e27cc7b9f5a74352ff3e4f47ea381c84b71a
Joshua Abu <joshujah007@gmail.com>
2016-02-25 11:59:06 -0500

    sidebar touches

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,45 +1,49 @@
 var $ = require("jquery");
 var handlebars = require('handlebars');
-var image;
-var price;
-var title;
-var currency_code;
-var manufacturer;
-var cat;
-var q = "tacos";
 
 
-var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords="+q+"&includes=Images,Shop";
+// $(".search").on('click', fetchJSONP());
 
-function fetchJSONP(url, callback) {
-    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    var script = document.createElement('script');
+    var image;
+    var price;
+    var title;
+    var currency_code;
+    var manufacturer;
+    var cat;
+    var q = "tacos";
 
-    window[callbackName] = function(data) {
-        delete window[callbackName];
-        document.body.removeChild(script);
-        callback(data);
-    };
+    var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords="+q+"&includes=Images,Shop";
 
-    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-    document.body.appendChild(script);
-}
-var source   = $("#category-template").html();
-var template = handlebars.compile(source);
+    function fetchJSONP(url, callback) {
+        var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+        var script = document.createElement('script');
 
-function logData(data){
-    var arrayData = data.results;
-    console.log(arrayData)
-    arrayData.forEach(function(value, index, thisArray){
-      var context = {
-        // cat: thisArray[index].category_path[0],
-        image: thisArray[index].Images[0].url_fullxfull,
-        price: thisArray[index].price,
-        title: thisArray[index].title,
-        currency_code: thisArray[index].currency_code,
-        manufacturer: thisArray[index].Shop.shop_name,
-      }
-      $(".category-list").append(template(context));
-    });
-}
-fetchJSONP(url, logData);
+        window[callbackName] = function(data) {
+            delete window[callbackName];
+            document.body.removeChild(script);
+            callback(data);
+        };
+
+        script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+        document.body.appendChild(script);
+    }
+    var source   = $("#category-template").html();
+    var template = handlebars.compile(source);
+
+    function logData(data){
+        var arrayData = data.results;
+        console.log(arrayData)
+        arrayData.forEach(function(value, index, thisArray){
+          var context = {
+            // cat: thisArray[index].category_path[0],
+            image: thisArray[index].Images[0].url_fullxfull,
+            price: thisArray[index].price,
+            title: thisArray[index].title,
+            currency_code: thisArray[index].currency_code,
+            manufacturer: thisArray[index].Shop.shop_name,
+          }
+          $(".category-list").append(template(context));
+        });
+
+    }
+    fetchJSONP(url, logData);
